45f9e48b1141cd153925b1272a73b82b4f7e86a7
Rebecca Xu <rebeccaxu@Rebeccas-MacBook-Pro-6.local>
2023-10-07 03:19:30 -0500

    Add Job Posting

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -11,9 +11,9 @@ function Navbar({ page }: PageName) {
     return (
       <div className="navbar">
           <img src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w"/>
-          <a className="active" href="feed">Feed</a>
-          <a href="resources">Resources</a>
-          <a href="opportunities">Opportunities</a>
+          <a className="active" href="../feed">Feed</a>
+          <a href="../resources">Resources</a>
+          <a href="../opportunities">Opportunities</a>
       </div>
     );
   }
@@ -21,9 +21,9 @@ function Navbar({ page }: PageName) {
     return (
       <div className="navbar">
           <img src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w"/>
-          <a href="feed">Feed</a>
-          <a className="active" href="resources">Resources</a>
-          <a href="opportunities">Opportunities</a>
+          <a href="../feed">Feed</a>
+          <a className="active" href="../resources">Resources</a>
+          <a href="../opportunities">Opportunities</a>
       </div>
     );
   }
@@ -31,9 +31,9 @@ function Navbar({ page }: PageName) {
     return (
       <div className="navbar">
           <img src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w"/>
-          <a href="feed">Feed</a>
-          <a href="resources">Resources</a>
-          <a className="active" href="opportunities">Opportunities</a>
+          <a href="../feed">Feed</a>
+          <a href="../resources">Resources</a>
+          <a className="active" href="../opportunities">Opportunities</a>
         {/* <UserButton/>
           <div>
 
