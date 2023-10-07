46d6e4726d29f575c9fb68c44e568c3293dca381
dsrasheed <daniyaalrasheed@gmail.com>
2023-10-06 21:23:37 -0500

    Added dummy user to database

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,7 +3,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    
+    console.log("Hello");
+    await prisma.user.create({
+        data: {
+            email: "johndoe@gmail.com",
+            firstname: "John",
+            lastname: "Doe",
+            phoneNumber: "111-111-1111"
+        }
+    });
+
 }
 
 main()
--- backend/src/routes/feed.ts
+++ backend/src/routes/feed.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import { Request, Response } from 'express';
-// import { prisma } from '../clients';
+import { prisma } from '../clients';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
     logger.log('info', 'Getting Posts...');
+
+    try {
+        const users = await prisma.user.findMany();
+        res.send(users).status(200).end();
+    } catch {
+        res.status(400).end();
+    }
+});
+
+router.post('/', async(req: Request, res: Response) => {
+    logger.log('info', 'Creating a new post');
     res.status(200).end();
 });
 
