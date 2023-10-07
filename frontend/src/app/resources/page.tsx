7de71654e87328c67ada011a7814363d6e9f3031
Adora Vaz <adora.vaz@gmail.com>
2023-10-07 08:06:33 -0500

    Finished HardSkills

--- frontend/src/app/resources/page.tsx
+++ frontend/src/app/resources/page.tsx
@@ -7,8 +7,8 @@ return (
   <main>
     <Navbar page="resources"/>
      <div className = "bodyResource">
-      <div className="resourcesContainer"> 
-         <h2 className="title">Rebuilding Aurora 101: What to Know On Your First Day</h2>
+      <div className= "resourcesContainer"> 
+         <h2 className= "heading-title">Rebuilding Aurora 101: What to Know On Your First Day</h2>
          <div className="imageContainer">
            <img src="../resourceImg/743 N View 2011 volunteer photo group.jpg" alt="Description of Image 1" className="resourceImage"/>
            <img src="../resourceImg/20170603_100630.jpg" alt="Description of Image 2" className="resourceImage"/>
@@ -17,33 +17,35 @@ return (
        <div><p className="imageContainer">Thank you for taking a step toward rebuilding together, here are tips to make your experience as smooth as possible!</p></div>
        </div>
        </div>
-       <div className="leftSquare"><h3 className="title">Painting 101:</h3><p className="textInsideSquare">For those, who will be taking part of the paint part of the rebuilding process:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/CRXCB_3gLok?si=EQ_HjPy_c0byTPoa" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="rightSquare"><h3 className="title">Woodworking 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with wood and tools for the rebuilding process:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/3cME3vK1aaQ?si=b6vwzcbA6GvKobum" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="leftSquare"><h3 className="title">Windows 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with window installation for the rebuilding process:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/1qAdrxsL0k8?si=1m8d2DY6QruoVJgL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="rightSquare"><h3 className="title">Drywall and Plaster 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with the drywall and plasterwork for the rebuilding process:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/McCZLfZipTg?si=HTi-EcSUD-f4kSvH" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="leftSquare"><h3 className="title">Flooring Installation 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with the flooring and installation process:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/lvqdlRzNQss?si=ubRHAlVt7j5Fymeq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="rightSquare"><h3 className="title">HVAC 101:</h3><p className="textInsideSquare">For those, who will be taking part of the HVAC process here are the basics:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/vJjaRTPiksA?si=egStAJTVLDWpbw-O" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="leftSquare"><h3 className="title">Plumbing 101:</h3><p className="textInsideSquare">For those, who will be taking part of the plumbing process here are the basics:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/SCmtGb5SFfU?si=Xo-yeLoIBC62fO9J" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="rightSquare"><h3 className="title">Electricity 101:</h3><p className="textInsideSquare">For those, who will be taking part of the electricity process here are the basics:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/zdrUX6_LGmE?si=mDnytQA1XgIhujtO" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-       </div>
-       <div className="leftSquare"><h3 className="title">Landscaping 101:</h3><p className="textInsideSquare">For those, who will be taking part of the landscaping process here are the basics:</p>
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/e-wO-LqEQb4?si=xeDmU3kERv6wkU8G" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-        </div> 
+       <div className='video-container'>
+          <div className="square"><h3 className="title">Painting 101:</h3><p className="textInsideSquare">For those, who will be taking part of the paint part of the rebuilding process:</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/CRXCB_3gLok?si=EQ_HjPy_c0byTPoa" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Woodworking 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with wood and tools for the rebuilding process:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/3cME3vK1aaQ?si=b6vwzcbA6GvKobum" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Windows 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with window installation for the rebuilding process:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/1qAdrxsL0k8?si=1m8d2DY6QruoVJgL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Drywall and Plaster 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with the drywall and plasterwork for the rebuilding process:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/McCZLfZipTg?si=HTi-EcSUD-f4kSvH" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Flooring Installation 101:</h3><p className="textInsideSquare">For those, who will be taking part of working with the flooring and installation process:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/lvqdlRzNQss?si=ubRHAlVt7j5Fymeq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">HVAC 101:</h3><p className="textInsideSquare">For those, who will be taking part of the HVAC process here are the basics:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/vJjaRTPiksA?si=egStAJTVLDWpbw-O" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Plumbing 101:</h3><p className="textInsideSquare">For those, who will be taking part of the plumbing process here are the basics:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/SCmtGb5SFfU?si=Xo-yeLoIBC62fO9J" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Electricity 101:</h3><p className="textInsideSquare">For those, who will be taking part of the electricity process here are the basics:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/zdrUX6_LGmE?si=mDnytQA1XgIhujtO" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+          <div className="square"><h3 className="title">Landscaping 101:</h3><p className="textInsideSquare">For those, who will be taking part of the landscaping process here are the basics:</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/e-wO-LqEQb4?si=xeDmU3kERv6wkU8G" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div> 
+        </div>
      </main>
 );
 }
