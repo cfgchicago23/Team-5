8a4fbe1ec7a8356675c51c9533803a25d351346e
Rebecca Xu <rebeccaxu@Rebeccas-MacBook-Pro-6.local>
2023-10-07 06:54:16 -0500

    Skill Video

--- frontend/src/app/components/JobPosting.tsx
+++ frontend/src/app/components/JobPosting.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import '../globals.css';
 import { useState } from 'react';
 import SignUp from './SignUp';
+import '../app.js'
 
 const JobPosting = () => {
     const [seen, setSeen] = useState<boolean>(false)
@@ -11,6 +12,7 @@ const JobPosting = () => {
     };
   return (
     <main>
+        <script src="../app.js" defer></script>
         <h1>Veterans' Fall Clean-Up and Repairs</h1>
         <hr></hr>
         <div className="row">
@@ -61,13 +63,32 @@ const JobPosting = () => {
                     Tutorial Videos
                 </div>
                 <div className="column right1">
-                    <a href="https://www.youtube.com/embed/CRXCB_3gLok?si=EQ_HjPy_c0byTPoa">
-                        Painting Basics <img className="video-icon" src="../../video-icon.svg"></img>
-                    </a>
+                    Painting Basics  <button id="myBtn">
+                    <img className="video-icon" src="../../video-icon.svg"></img>
+                    </button>
+                    <div id="myModal" className="modal">
+
+                    <div className="modal-content">
+                        <span className="close">X</span>
+                        <div><h3 className="title">Painting 101:</h3>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/CRXCB_3gLok?si=EQ_HjPy_c0byTPoa" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        </div>
+                    </div>
+                    </div>
                     <br></br>
-                    <a href="https://www.youtube.com/embed/CRXCB_3gLok?si=EQ_HjPy_c0byTPoa">
-                        Safety Overview <img className="video-icon" src="../../video-icon.svg"></img>
-                    </a>
+
+                    {/* Floor Installation  <button id="myBt2n">
+                    <img className="video-icon" src="../../video-icon.svg"></img>
+                    </button>
+                    <div id="myModal2" className="modal">
+
+                    <div className="modal-content">
+                        <span className="close2">X</span>
+                        <div><h3 className="title">Flooring Installation 101:</h3>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/lvqdlRzNQss?si=ubRHAlVt7j5Fymeq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        </div>
+                    </div>
+                    </div> */}
                 </div>
             </div>
             <div className="row">
@@ -75,14 +96,15 @@ const JobPosting = () => {
                 </div>
                 {seen ? <SignUp toggle={togglePop}/> : 
                 <div className='column right1'>
-                    <div onClick={togglePop}>
-                        <button>Sign Up For This Event!</button>
+                    <div className="green-button" onClick={togglePop}>
+                        Sign Up For This Event!
                     </div>
                 </div>}
             </div>
         </div>
         <div className="column right2">Comment Thread</div>
         </div>
+    <script src="../app.js" ></script> 
     </main>
   );
 };
