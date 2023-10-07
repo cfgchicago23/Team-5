fe4a50c27a82eb4374fecf32e6c3671f4016a616
Adora Vaz <adora.vaz@gmail.com>
2023-10-06 21:22:43 -0500

    Base for resources

--- frontend/src/app/resources/page.tsx
+++ frontend/src/app/resources/page.tsx
@@ -1,11 +1,19 @@
 import Navbar from '../components/Navbar'
-import '../globals.css'
+import '../globals.css';
 
-export default function Resources() {
-  return (
-    <main>
-      <Navbar page="resources"/>
-      This is the Resources Page
-    </main>
-    )
-  }
+
+const Resources = () => {
+return (
+  <main>
+    <Navbar page="feed"/>
+    <div>
+      <h1>Resources</h1>
+      <p>Rebuilding Aurora 101: What to Know On Your First Day</p>
+      <p>Let's say you are assigned a painting job, what is your first step?</p>
+      </div> 
+  </main>
+);
+}
+
+
+export default Resources;
