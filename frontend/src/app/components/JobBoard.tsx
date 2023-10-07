6038b642e1b455e567222c36024052787944d773
sammyfalvey <falvey.s@wustl.edu>
2023-10-07 01:47:52 -0500

    job postings

--- frontend/src/app/components/JobBoard.tsx
+++ frontend/src/app/components/JobBoard.tsx
@@ -4,32 +4,37 @@ const JobBoard = () => {
   return (
     <main>
         <div className="recent-event">
-            <div className="container">
-                 <h2 className="recent-event-heading">UPCOMING EVENTS</h2>
+            <h2 className="recent-event-heading">UPCOMING EVENTS</h2>
             <p className="recent-event-sub-heading">Sign up to volunteer at an event</p>
-            </div>
-
         </div>
-        <div className="width-50">
-                <div className="recent-event-list">
-                    <div className="width-100">
-                    <h4 className="event-title">Veterans Fall Clean-Up and Repairs</h4>
-                    <p className="event-company">Saturday, November 11, 2023 <i className="fa fa-star" aria-hidden="true"></i> 8:00 AM - 1:00 PM </p>
-                    </div>
-                    <div className="width-33">
-                    <i className="fa fa-briefcase" aria-hidden="true"></i> Rebuilding Together Map
-                    </div>
-                    <div className="width-100">
-                    <p className="event-skill">
-                    Join Rebuilding Together Aurora and Crossroads Church as we team up to serve those who have served and military families in the Fox Valley in honor of the sacrifices they have made to our country. Last year a record-breaking number of volunteers served more Veterans, military families and senior citizens than ever before and we are hoping to surpass those numbers in 2023.
-                    </p>
-                    <p>Register your team HERE to be paired with a family in need.</p>
-                    <p>Do you know a Veteran, active-duty military, Gold Star family or service member’s widow in need of fall yard clean-up or light repairs? Email support@rtaurora.org</p>
-                    </div>
-                    <div className="width-100">
-                    <a href="#" className="event-apply-button">Sign Up Now</a>
-                </div>
-            </div>
+          <div className="recent-event-list">
+              <div className='event-1'>
+                <img classname = 'job-images' src = '../images/job-pic1.png' alt='happy volunteers'></img>
+                <h4 className="event-title">Veterans Fall Clean-up and Repairs</h4>
+                <p className="event-time">Saturday, November 25, 2023 9:00 AM - 2:00 PM </p>
+                <p className="event-skill">
+                Join Rebuilding Together Aurora and Crossroads Church as we team up to serve those who have served and military families in the Fox Valley.
+                </p>
+
+              </div>
+              <div className='event-1'>
+                <img classname = 'job-images' src = '../images/job-pic2.png' alt='happy volunteers'></img>
+                <h4 className="event-title">Community Center Floor & Painting</h4>
+                <p className="event-time">Saturday, November 11, 2023 8:00 AM - 1:00 PM </p>
+                <p className="event-skill">
+                Welcoming all skills levels to re-paint and re-floor Aurora Community Center! 
+                </p>
+                <br></br>
+              </div>
+              <div className='event-1'>
+                <img classname = 'job-images' src = '../images/job-pic3.png' alt='happy volunteers'></img>
+                <h4 className="event-title">South Neighboorhood Home Repair & Cleaning</h4>
+                <p className="event-time">Saturday, December 2, 2023 8:30 AM - 1:00 PM </p>
+                <p className="event-skill">
+                Help improve home safety features for seniors in your neighborhood to prevent falls and clear space for mobility.
+                </p>
+   
+              </div>
         </div>
     </main>
   );
