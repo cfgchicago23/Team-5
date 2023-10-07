aa2a8635855587a3e652e344c11f1a19b1162953
Rebecca Xu <rebeccaxu@Rebeccas-MacBook-Pro-6.local>
2023-10-06 22:08:27 -0500

    Fixing Bugs

--- team-5/src/app/components/Navbar.tsx
+++ team-5/src/app/components/Navbar.tsx
@@ -1,17 +1,7 @@
 import '../globals.css';
-import { UserButton , SignInButton,SignUpButton} from "@clerk/nextjs";
+// import { UserButton , SignInButton,SignUpButton} from "@clerk/nextjs";
 
-<<<<<<< HEAD
-const Navbar = () => {
-  return (
-    <div className="topnav">
-        <img src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w"/>
-        <a href="#feed">Feed</a>
-        <a href="#resources">Resources</a>
-        <a className="active" href="#opportunities">Opportunities</a>
-    </div>
-  );
-=======
+
 interface PageName {
   page: string
 }
@@ -44,20 +34,15 @@ function Navbar({ page }: PageName) {
           <a href="feed">Feed</a>
           <a href="resources">Resources</a>
           <a className="active" href="opportunities">Opportunities</a>
-        <UserButton/>
+        {/* <UserButton/>
           <div>
 
 <SignInButton/>
 <SignUpButton/>
-</div>
+</div> */}
       </div>
     );
   }
-<<<<<<< HEAD
->>>>>>> a88cd60bcd66a1912aa330098c861fb541d8df31
-=======
-
->>>>>>> f63749b692eabaeb006d627e67ea0cafce7b44dd
-};
+}
 
 export default Navbar;
--- team-5/src/app/opportunities/page.tsx
+++ team-5/src/app/opportunities/page.tsx
@@ -8,6 +8,6 @@ export default function Opportunities() {
       <Navbar page="opportunities"/>
       <JobBoard/>
       This is the Opportunities Page
-    </ma
+    </main>
     )
   }
