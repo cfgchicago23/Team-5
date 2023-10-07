dc07b02921c5edce4318a1af1c056b8219a1492b
Rebecca Xu <rebeccaxu@Rebeccas-MacBook-Pro-6.local>
2023-10-06 19:54:11 -0500

    Making header image responsive

--- team-5/src/app/components/Navbar.tsx
+++ team-5/src/app/components/Navbar.tsx
@@ -2,14 +2,12 @@ import '../globals.css';
 
 const Navbar = () => {
   return (
-    <body>
-    <div className="topnav">
-        <img src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w"/>
+    <div className="navbar">
+        <img className="h-70 lg:h-40" src="http://images.squarespace-cdn.com/content/v1/55e48a8ce4b0a8e051b2aea3/1581533791977-LXZWE6Q3JU2GPGEGK0QQ/RTA+logo+2.JPG?format=1500w"/>
         <a href="#feed">Feed</a>
         <a href="#resources">Resources</a>
         <a className="active" href="#opportunities">Opportunities</a>
     </div>
-    </body>
   );
 };
 
