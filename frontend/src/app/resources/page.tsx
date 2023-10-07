fe5dd135e6254638f66fa5a1ab1a83fdd3742f33
Adora Vaz <adora.vaz@gmail.com>
2023-10-06 22:39:16 -0500

    Imgs for resources

--- frontend/src/app/resources/page.tsx
+++ frontend/src/app/resources/page.tsx
@@ -6,11 +6,14 @@ const Resources = () => {
 return (
   <main>
     <Navbar page="feed"/>
-    <div>
-      <h1>Resources</h1>
-      <p>Rebuilding Aurora 101: What to Know On Your First Day</p>
-      <p>Let's say you are assigned a painting job, what is your first step?</p>
+    <div className="resourcesContainer"> 
+        <h1 className="title">Rebuilding Aurora 101: What to Know On Your First Day</h1>
+        <div className="imageContainer">
+          <img src="../resourceImg/743 N View 2011 volunteer photo group.jpg" alt="Description of Image 1" className="resourceImage" />
+          <img src="../resourceImg/20170603_100630.jpg" alt="Description of Image 2" className="resourceImage"/>
+          <img src="../resourceImg/20180703_151710.jpg" alt="Description of Image 3" className="resourceImage"/>
       </div> 
+      </div>
   </main>
 );
 }
